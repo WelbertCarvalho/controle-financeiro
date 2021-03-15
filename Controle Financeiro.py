80f3aafd19c72fd2dcb4659f5f765be53e736d3b
WelbertCarvalho <41022499+WelbertCarvalho@users.noreply.github.com>
2021-03-15 12:28:23 -0300

    Add files via upload

--- Controle Financeiro.py
+++ Controle Financeiro.py
@@ -5,4 +5,4 @@ receitas = func.lancamento('receitas')
 despesas = func.lancamento('despesas')
 func.saldo(receitas, despesas)
 
-
+input('Pressione Enter para sair...')
--- funcoes_controle_financeiro.py
+++ funcoes_controle_financeiro.py
@@ -76,11 +76,11 @@ def saldo(receitas, despesas):
 	if saldo > 0:
 		comprometido = round((despesas/receitas) * 100, 2)
 		if comprometido >= 70:
-			print('Seu saldo é R$ '+ str(saldo) + ' e está positivo! Porém, você está com ' + str(comprometido) + ' % da sua renda comprometida. Tente reduzir custos.')
+			print('Seu saldo é R$ '+ str(saldo) + ', está positivo! Porém, você está com ' + str(comprometido) + ' % da sua renda comprometida. Tente reduzir custos.')
 		elif (comprometido < 70) & (comprometido >= 50):
-			print('Seu saldo é R$ '+ str(saldo) + ' e está positivo! Você está com ' + str(comprometido) + ' % da sua renda comprometida. Sua saúde financeira está regular.')
+			print('Seu saldo é R$ '+ str(saldo) + ', está positivo! Você está com ' + str(comprometido) + ' % da sua renda comprometida. Sua saúde financeira está regular.')
 		elif (comprometido < 50):
-			print('Seu saldo é R$ '+ str(saldo) + ' e está positivo e você é um mestre das financas. Está com ' + str(comprometido) + ' % da sua renda comprometida. Nos mostre seu segredo, Mr. M.')
+			print('Seu saldo é R$ '+ str(saldo) + ', está positivo e você é um mestre das financas. Está com ' + str(comprometido) + ' % da sua renda comprometida. Nos mostre seu segredo, Mr. M.')
 	else:
 		print('Seu saldo é negativo. Você está devendo R$ ' + str(saldo*(-1)) + '. Você deve Você deve cortar custos imediatamente. Vou te apresentar ao Julius.')
 
