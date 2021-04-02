cb4ef3364ca1b406bfdd11e1d9845b4436da2385
WelbertCarvalho <41022499+WelbertCarvalho@users.noreply.github.com>
2021-04-02 19:22:47 -0300

    Delete Controle Financeiro.py

--- Controle Financeiro.py
+++
@@ -1,8 +0,0 @@
-import funcoes_controle_financeiro as func
-
-
-receitas = func.lancamento('receitas')
-despesas = func.lancamento('despesas')
-func.saldo(receitas, despesas)
-
-input('Pressione Enter para sair...')
